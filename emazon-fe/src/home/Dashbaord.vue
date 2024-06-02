caa6e64df6a57b0f56f1186190858815b51f76cb
Azaz50 <azazmohammad14@gmail.com>
2024-06-02 14:44:42 +0530

    in admin panel ui change

--- emazon-fe/src/home/Dashbaord.vue
+++ emazon-fe/src/home/Dashbaord.vue
@@ -49,7 +49,7 @@
       </div>
 </template>
 
-<!-- 
+
 <script>
     // On page load or when changing themes, best to add inline in `head` to avoid FOUC
     if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
@@ -57,4 +57,4 @@
     } else {
         document.documentElement.classList.remove('dark')
     }
-</script> -->
+</script>
