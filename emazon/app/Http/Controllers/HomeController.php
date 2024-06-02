f2b265e6e63c1e997d08eb6e1889c29f599bd431
Azaz50 <azazmohammad14@gmail.com>
2024-06-02 14:46:58 +0530

    HomeController code changes

--- emazon/app/Http/Controllers/HomeController.php
+++ emazon/app/Http/Controllers/HomeController.php
@@ -2,24 +2,40 @@
 
 namespace App\Http\Controllers;
 
-use mail;
 use App\Models\User;
 use App\Models\Product;
 use App\Mail\UserRegistered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Log;
 
 class HomeController extends Controller
 {
     public function index(Request $request)
     {
+        // Retrieve the user with ID 1
         $user = User::find(1);
-        $registration_mail = new UserRegistered($user);
-        Mail::to($user)->send($registration_mail);
-        
-        $query = Product::query();
-        $products = $query->take(12)->get();
 
+        if ($user) {
+            // Prepare the registration mail
+            $registration_mail = new UserRegistered($user);
+
+            // Try to send the mail and catch any potential exceptions
+            try {
+                Mail::to($user)->send($registration_mail);
+            } catch (\Exception $e) {
+                // Log the error message for debugging
+                Log::error('Failed to send registration email: ' . $e->getMessage());
+            }
+        } else {
+            // Handle the case where the user is not found
+            Log::warning('User with ID 1 not found');
+        }
+
+        // Fetch the latest 12 products
+        $products = Product::latest()->take(12)->get();
+
+        // Return the home view with the products data
         return view('home', [
             'products' => $products,
         ]);
